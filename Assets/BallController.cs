b7a2b526887a7ff0c4f530e853bba086a8d9f337
mete2001 <metecil09@gmail.com>
2025-01-23 13:43:15 -0800

    Added movement logic to sphere using WASD inputs

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -2,8 +2,10 @@ using UnityEngine;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Rigidbody sphereRigidbody;
+    public float ballSpeed = 2f;
     void Start()
+    
     {
         Debug.Log("Calling the Start method");
     }
@@ -12,5 +14,31 @@ public class NewMonoBehaviourScript : MonoBehaviour
     void Update()
     {
         Debug.Log("Calling the Update method");
+        Vector2 inputVector = Vector2.zero;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            inputVector += Vector2.up;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            inputVector += Vector2.down;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputVector += Vector2.right;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputVector += Vector2.left;
+        }
+        
+        // (same input checks as above)
+
+        // Convert (x,y) to (x,0,z) in 3D
+        Vector3 inputXZPlane = new(inputVector.x, 0, inputVector.y);
+
+        // Apply force to the rigidbody
+        sphereRigidbody.AddForce(inputXZPlane * ballSpeed);
     }
 }
