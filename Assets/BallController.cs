e2bc152b07a324c54620b012e25c4dcb11921b84
mete2001 <metecil09@gmail.com>
2025-01-23 13:23:28 -0800

    added test logs to see functionality

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -5,12 +5,12 @@ public class NewMonoBehaviourScript : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Debug.Log("Calling the Start method");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log("Calling the Update method");
     }
 }
